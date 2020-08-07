7a18cb01ce58c36134471697afd051c8fd74cfa4
lush93 <lush93@outlook.com>
2020-08-07 18:35:44 +0800

    88. Merge Sorted Array Runtime: 164 ms, faster than 97.12% of Kotlin online submissions for Merge Sorted Array. Memory Usage: 34.1 MB, less than 75.00% of Kotlin online submissions for Merge Sorted Array.

--- src/MergeSortedArray.kt
+++ src/MergeSortedArray.kt
@@ -1,7 +1,26 @@
 class MergeSortedArray {
     fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
-        for(i in 0 until n)
-            nums1[m + i] = nums2[i]
-        nums1.sort()
+        val L = IntArray(m + 1)
+        for (i in 0 until m)
+            L[i] = nums1[i]
+        L[m] = Int.MAX_VALUE
+
+        val R = IntArray(n + 1)
+        for (i in 0 until n)
+            R[i] = nums2[i]
+        R[n] = Int.MAX_VALUE
+
+        var i = 0
+        var j = 0
+
+        for (k in 0 until m + n) {
+            if (L[i] <= R[j]) {
+                nums1[k] = L[i]
+                i++
+            } else {
+                nums1[k] = R[j]
+                j++
+            }
+        }
     }
 }
