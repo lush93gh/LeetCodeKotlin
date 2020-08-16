afdbe1fd0a6e6fa97638cfc712257241ce5086cf
lush93 <lush93@outlook.com>
2020-08-16 22:40:20 +0800

    70. Climbing Stairs Runtime: 120 ms, faster than 95.67% of Kotlin online submissions for Climbing Stairs. Memory Usage: 31.5 MB, less than 63.64% of Kotlin online submissions for Climbing Stairs.

--- src/ClimbingStairs.kt
+++ src/ClimbingStairs.kt
@@ -1,19 +1,11 @@
 class ClimbingStairs {
-    lateinit var cache: HashMap<Int, Int>
+    private val cache = HashMap<Int, Int>()
 
     fun climbStairs(n: Int): Int {
-        cache = HashMap<Int, Int>()
-        return climb(n, 0)
-    }
+        if(cache.containsKey(n)) return cache[n]!!
 
-    fun climb(n: Int, steps: Int): Int{
-        if(cache.containsKey(steps)) return cache[steps]!!
-
-        if(n- steps == 1 || n==steps) return 1
-        else if(steps > n) return 0
-
-        val result = climb(n, steps + 1) + climb(n, steps +2)
-        cache[steps] = result
+        val result = if(n<=2) n else climbStairs(n-1) + climbStairs(n-2)
+        cache[n] = result
 
         return result
     }
