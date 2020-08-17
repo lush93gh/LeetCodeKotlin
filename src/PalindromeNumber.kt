f305c2284d2e0d314d0cc18dcdb874dd52d57533
lush93 <lush93@outlook.com>
2020-08-17 15:17:14 +0800

    9. Palindrome Number Runtime: 216 ms, faster than 74.91% of Kotlin online submissions for Palindrome Number. Memory Usage: 33.8 MB, less than 77.74% of Kotlin online submissions for Palindrome Number.

--- src/PalindromeNumber.kt
+++ src/PalindromeNumber.kt
@@ -1,5 +1,23 @@
+import kotlin.math.log10
+import kotlin.math.roundToInt
+
 class PalindromeNumber {
     fun isPalindrome(x: Int): Boolean {
-        return false
+        if(x< 0) return false
+
+        var number = x
+        val digits = mutableListOf<Int>()
+
+        while(number > 0){
+            digits.add(number%10)
+            number /= 10
+        }
+
+        for(i in 0 until digits.size/2){
+            if(digits[i] != digits[digits.size -1 -i])
+                return false
+        }
+
+        return true
     }
 }
