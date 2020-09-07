dde29214f2555d33eb88598a8ff18a52a0b504c6
lush93 <lush93@outlook.com>
2020-09-07 23:47:56 +0800

    34. Find First and Last Position of Element in Sorted Array

--- src/SearchForARange.kt
+++ src/SearchForARange.kt
@@ -1,43 +1,36 @@
 class SearchForARange {
     fun searchRange(nums: IntArray, target: Int): IntArray {
+        if(nums.isEmpty()) return intArrayOf(-1, -1)
+
         var pivot: Int
         var left = 0
         var right = nums.size - 1
-        var hasFound = false
         val result = intArrayOf(-1, -1)
 
         while(left < right){
             pivot = left + (right - left) /2
             when {
-                nums[pivot] == target ->{
-                    hasFound = true
-                    right = pivot
-                }
-                target < nums[pivot] ->
-                    right = pivot
-                else ->
-                    left = pivot +1
+                nums[pivot] == target -> right = pivot
+                target < nums[pivot] -> right = pivot
+                else -> left = pivot +1
             }
         }
 
-        if(!hasFound) return result
-        else{
+        if(nums[right] == target){
             result[0] = left
-            right = nums.size - 1
+            right = nums.size
 
-            while(left <= right){
+            while(left < right){
                 pivot = left + (right - left) /2
                 when {
                     nums[pivot] == target -> left = pivot + 1
-                    target < nums[pivot] ->
-                        right = pivot - 1
-                    else ->
-                        left = pivot + 1
+                    target < nums[pivot] -> right = pivot
+                    else -> left = pivot + 1
                 }
             }
-
-
+            result[1] = left - 1
         }
+
         return result
     }
 }
