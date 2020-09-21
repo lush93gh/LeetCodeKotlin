6e4a33452f54546881d7252cebecf86165e15c0d
lush93 <lush93@outlook.com>
2020-09-21 14:17:26 +0800

    Search in a Binary Search Tree

---
+++ src/binarysearchtree/SearchInABinarySearchTree.kt
@@ -0,0 +1,17 @@
+package binarysearchtree
+
+import TreeNode
+
+class SearchInABinarySearchTree {
+    fun searchBST(root: TreeNode?, `val`: Int): TreeNode? {
+        root?.let{
+            val value = root.`val`
+            return when {
+                `val` == value -> root
+                `val` > value -> searchBST(root.right, `val`)
+                else -> searchBST(root.left, `val`)
+            }
+        }
+        return root
+    }
+}
