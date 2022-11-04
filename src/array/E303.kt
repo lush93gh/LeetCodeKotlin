ebb5754a91de0ffbe1b529303da423e7d23d0f19
johnlu <johnlu@seekrtech.com>
2022-11-04 21:36:25 +0800

    303. Range Sum Query - Immutable

---
+++ src/array/E303.kt
@@ -0,0 +1,9 @@
+package array
+
+class E303(nums: IntArray) {
+    private val array =  nums
+    fun sumRange(left: Int, right: Int): Int {
+        return array.sliceArray(left..right).sum()
+    }
+
+}
