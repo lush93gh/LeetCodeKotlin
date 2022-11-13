5afc96bd2f5574cd06f6d7d078207617c31f5660
johnlu <johnlu@seekrtech.com>
2022-11-13 21:55:43 +0800

    699. Falling Squares

--- src/Solution.kt
+++ src/Solution.kt
@@ -1,22 +1,18 @@
 import array.H218
+import array.H699
 
 class Solution {
-    companion object{
+    companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            var c = H218()
-            val a = c.getSkyline(
+            var c = H699()
+            val a = c.fallingSquares(
                 arrayOf(
-//                    intArrayOf(2,9,10),
-//                    intArrayOf(3,7,15),
-//                    intArrayOf(5,12,12),
-//                    intArrayOf(15,20,10),
-//                    intArrayOf(19,24,8)
-                 //   intArrayOf(0,2147483647,2147483647)
-                    intArrayOf(0,3,3),
-                    intArrayOf(1,5,3),
-                    intArrayOf(2,4,3),
-                    intArrayOf(3,7,3)
+                    intArrayOf(4, 1),
+                    intArrayOf(6, 9),
+                    intArrayOf(6, 8),
+                    intArrayOf(1, 9),
+                    intArrayOf(9, 8)
                 )
             )
             println(a)
@@ -26,4 +22,9 @@ class Solution {
 
 }
 
+//[[1,2],[2,3],[6,1]]
+//[[100,100],[200,100]]
+//[[9,7],[1,9],[3,1]]
+//[[6,1],[9,2],[2,4]]
+//[[4,1],[6,9],[6,8],[1,9],[9,8]]
 
