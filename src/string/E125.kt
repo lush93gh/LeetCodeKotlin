2f84e3b9a9bd7febc9bd7c2a1fb9ea85841dfcc4
johnlu <johnlu@seekrtech.com>
2022-11-16 22:53:56 +0800

    125. Valid Palindrome

--- src/string/E125.kt
+++ src/string/E125.kt
@@ -1,5 +1,17 @@
-class ValidPalindrome {
+package string
+
+class E125 {
     fun isPalindrome(s: String): Boolean {
+        val regex = Regex("[^A-Za-z0-9]")
+        val ans = regex.replace(s, "").toLowerCase()
+
+        for (index in 0 until ans.length / 2) {
+            if (ans[index] != ans[ans.length - 1 - index]) return false
+        }
+        return true
+    }
+
+    fun isPalindrome2(s: String): Boolean {
         val regex = """([a-z0-9])""".toRegex()
         val result = regex.findAll(s.toLowerCase())
         val array = mutableListOf<MatchResult>()
