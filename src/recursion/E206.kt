b55bfcd5d59aa16eaddd63d8b1f1af8da1b02170
johnlu <johnlu@seekrtech.com>
2022-11-18 23:40:04 +0800

    206. Reverse Linked List

---
+++ src/recursion/E206.kt
@@ -0,0 +1,20 @@
+package recursion
+
+import ListNode
+
+class E206 {
+    var result: ListNode? = null
+    fun reverseList(head: ListNode?): ListNode? {
+        recurse(head)
+        head?.next = null
+        return result
+    }
+
+    fun recurse(head: ListNode?) {
+        if (head?.next != null) {
+            recurse(head.next)
+            head.next?.next = head
+        }
+        else result = head
+    }
+}
