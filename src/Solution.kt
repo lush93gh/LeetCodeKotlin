0996a243d8dee2fb222ce46840a027f385f3a97f
johnlu <johnlu@seekrtech.com>
2023-02-05 18:04:20 +0800

    564. Find the Closest Palindrome

--- src/Solution.kt
+++ src/Solution.kt
@@ -1,21 +1,15 @@
-import math.M2217
+import math.H564
 
 class Solution {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val a = M2217()
-            println(a.scaleCount(1234, 7))
-            val result = a.kthPalindrome(
-                intArrayOf(1,2,3,4,5,6,7,8,9,10, 11, 20,89,90,91,1234,9000,9001),
-                7
-            ).forEach {
-                print("$it,")
-            }
+            val a = H564()
+            println(a.nearestPalindromic("5000"))
+
         }
     }
 }
-//1000001,1001001,1002001,1003001,1004001,1005001,1006001,1007001,1008001,1009001,1010101,1019101,1088801,1089801,1090901,2233322,9999999,-1
 
 
 
---
+++ src/math/H564.kt
@@ -0,0 +1,85 @@
+package math
+
+import kotlin.math.ceil
+import kotlin.math.min
+
+class H564 {
+    fun nearestPalindromic(n: String): String {
+        val nLong = n.toLong()
+        var resultMinus = n
+        while (resultMinus.toLong() >= nLong || !isPalindromic(resultMinus)) {
+            val attempt = palindromicArithmetic(resultMinus, -1)
+            if (attempt.toLong() >= nLong) continue
+            else {
+                resultMinus = attempt
+            }
+        }
+        var resultPlus = n
+        while (resultPlus.toLong() <= nLong || !isPalindromic(resultPlus)) {
+            val attempt = palindromicArithmetic(resultPlus, 1)
+            if (attempt.toLong() <= nLong) continue
+            else {
+                resultPlus = attempt
+            }
+        }
+        val differenceMinus = nLong - resultMinus.toLong()
+        val differencePlus = resultPlus.toLong() - nLong
+
+        return if (differenceMinus <= differencePlus) resultMinus else resultPlus
+    }
+
+    private fun isPalindromic(n: String): Boolean {
+        val a = n.toCharArray()
+        for (i in 0..a.size / 2) {
+            if (a[i] != a[a.size - 1 - i]) return false
+        }
+        return true
+    }
+
+    private fun palindromicArithmetic(n: String, num: Int): String {
+        val nLong = n.toLong()
+        if (num > 0) {
+            if (findPalindromic(n).toLong() > nLong) return findPalindromic(n)
+            else {
+                return if (nLong + num < 10) (nLong + num).toString()
+                else if ((nLong + num) == 10L) "11"
+                else {
+                    val upper = n.substring(0, ceil(n.length.toFloat() / 2).toInt()).toLong()
+                    val lower = n.substring(ceil(n.length.toFloat() / 2).toInt(), n.length)
+                    val a = (upper + num).toString()
+                    var result = a
+                    for (i in lower.length - 1 downTo 0) {
+                        val index = min(i, a.length -1)
+                        result += a.toCharArray()[index]
+                    }
+                    result
+                }
+            }
+        } else {
+            if (findPalindromic(n).toLong() < nLong) return findPalindromic(n)
+            else {
+                return if (nLong + num < 10) (nLong + num).toString()
+                else if ((nLong + num) == 10L) "9"
+                else {
+                    val upper = n.substring(0, ceil(n.length.toFloat() / 2).toInt()).toLong()
+                    val lower = n.substring(ceil(n.length.toFloat() / 2).toInt(), n.length)
+                    val a = (upper + num).toString()
+                    var result = a
+                    for (i in lower.length - 1 downTo 0) {
+                        val index = min(i, a.length -1)
+                        result += a.toCharArray()[index]
+                    }
+                    result
+                }
+            }
+        }
+    }
+
+    private fun findPalindromic(n: String): String {
+        val a = n.toCharArray()
+        for (i in 0..a.size / 2) {
+            a[a.size - 1 - i] = a[i]
+        }
+        return String(a)
+    }
+}
