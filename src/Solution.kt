e2042ec2202e444f2be64e71b645cbc36fa7ae8d
johnlu <johnlu@seekrtech.com>
2023-02-13 21:52:45 +0800

    704. Binary Search

--- src/Solution.kt
+++ src/Solution.kt
@@ -1,12 +1,12 @@
-import string.M5
+import binarysearch.E704
 
 class Solution {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val a = M5()
-            println(a.longestPalindrome("bananas"))
-
+            val a = E704()
+            println(a.search(intArrayOf(-1,0,3,5,9,12), 9))
+           // (0..1).toList().binarySearch {  }
         }
     }
 }
---
+++ src/binarysearch/E704.kt
@@ -0,0 +1,36 @@
+package binarysearch
+
+class E704 {
+    fun search1(nums: IntArray, target: Int): Int {
+        var pivot: Int
+        var left = 0
+        var right = nums.size - 1
+
+        while(left <= right){
+            pivot = left + (right - left) /2
+            when {
+                nums[pivot] == target -> return pivot
+                target < nums[pivot] ->
+                    right = pivot - 1
+                else ->
+                    left = pivot +1
+            }
+        }
+        return -1
+    }
+    fun search(nums: IntArray, target: Int): Int {
+        var low = 0
+        var high = nums.size - 1
+
+        while (low <= high) {
+            val mid = (low + high).ushr(1)
+            val midVal = nums[mid]
+            if (target < midVal)
+                high = mid - 1
+            else if (target > midVal)
+                low = mid + 1
+            else return mid
+        }
+        return -1
+    }
+}
