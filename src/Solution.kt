94962d81a7a69b4a65b3b4ee56aae405144a2907
johnlu <johnlu@seekrtech.com>
2023-02-28 21:53:33 +0800

    454. 4Sum II

--- src/Solution.kt
+++ src/Solution.kt
@@ -1,14 +1,21 @@
-import array.M16
+import array.M454
 
 class Solution {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val a = M16()
+            val a = M454()
             val list = intArrayOf(
-                0,0,0
+                0, 0, 0
             )
-            println(a.threeSumClosest(list, 1))
+            println(
+                a.fourSumCount(
+                    intArrayOf(0,1,-1),
+                    intArrayOf(-1,1,0),
+                    intArrayOf(0,0,1),
+                    intArrayOf(-1,1,1)
+                )
+            )
         }
     }
 }
